f42b5f44c02eebb0dcb7bc5b8816de98a9c38749
Sergio Becerra <40771143+poobarfoob@users.noreply.github.com>
2021-01-09 10:39:03 -0800

    Add files via upload

---
+++ Windows/List Available Network Interfaces.bat
@@ -0,0 +1,5 @@
+@echo off
+echo This script is used to list all available interfaces that can be used.
+timeout 5
+l2tunnel.exe list
+pause
---
+++ Windows/Start Tunneling.bat
@@ -0,0 +1,2 @@
+l2tunnel.exe tunnel "YOUR INTERFACE HERE" -d XEMU:MAC:ADDRESS:HERE:XX:XX 0.0.0.0 9368  127.0.0.1 9367
+pause
