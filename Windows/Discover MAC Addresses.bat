ded93917e8264c4d7677ee42d16d9ce596679ecc
Sergio Becerra <40771143+poobarfoob@users.noreply.github.com>
2021-01-10 00:07:27 -0800

    Delete Discover MAC Addresses.bat

--- Windows/Discover MAC Addresses.bat
+++
@@ -1,6 +0,0 @@
-@echo OFF
-echo This should be used to test if you have the right interface chosen. Usually when you run this with the correct interface you'll see a bunch of MAC Addresses getting spat out by l2tunnel.
-
-set /p=Hit ENTER to continue...
-
-l2tunnel.exe discover "YOUR ADAPTER HERE"
